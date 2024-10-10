2a3b5f22ad918cbd35523f0d4eafe911a9a79655
Emanuel Gloria <emangloria@gmail.com>
2024-10-10 21:35:41 +0800

    Added external data

---
+++ QuickStart.R
@@ -0,0 +1,11 @@
+#########################################################################
+#########################################################################
+# External Datasets                                                     #
+#########################################################################
+#########################################################################
+
+spwh3<-read.table('spwh3.txt', header=FALSE,na.strings="NA", dec=".")
+cashdat<-read.table('cashdat.txt', header=FALSE,na.strings="NA", dec=".")
+sero<-read.table('sero1.txt', header=FALSE,na.strings="NA", dec=".")
+spwh2<-read.table('spwh2.txt', header=FALSE, 
+                  na.strings="NA", dec=".")
