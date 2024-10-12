5f0f9b1dc8cf649332d8c84323e820ce0b9d8b06
Emanuel Gloria <emangloria@gmail.com>
2024-10-12 13:42:23 +0800

    Added test line on file.

--- QuickStart.R
+++ QuickStart.R
@@ -9,3 +9,5 @@ cashdat<-read.table('cashdat.txt', header=FALSE,na.strings="NA", dec=".")
 sero<-read.table('sero1.txt', header=FALSE,na.strings="NA", dec=".")
 spwh2<-read.table('spwh2.txt', header=FALSE, 
                   na.strings="NA", dec=".")
+
+mean(spwh3$V2)
