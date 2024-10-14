127ef247772b05ddb01e233ee40b296c4fca89d0
Emanuel Gloria <emangloria@gmail.com>
2024-10-14 19:49:59 +0800

    Completed Practical Session 1 (QuickStart File)

--- QuickStart.R
+++ QuickStart.R
@@ -10,4 +10,69 @@ sero<-read.table('sero1.txt', header=FALSE,na.strings="NA", dec=".")
 spwh2<-read.table('spwh2.txt', header=FALSE, 
                   na.strings="NA", dec=".")
 
-mean(spwh3$V2)
+##########################################################################
+##########################################################################
+## QUICKT START                                                         ##
+##########################################################################
+##########################################################################
+
+###############################################
+# Normal distribution                         #
+###############################################
+
+rnorm(100,0,1)   # This function takes in number of values, mean and sd. Outputs an array.
+x<-rnorm(100,0,1)
+x
+mean(x)
+var(x)
+hist(x)
+
+
+x<-rnorm(10000,0,1)
+mean(x)
+var(x)
+hist(x,nclass=50) # This nclass here refers to the bins/bars in the plot
+
+
+x1<-rnorm(10000,0,1)
+x2<-rnorm(10000,1,1)
+par(mfrow=c(2,1))
+hist(x1,nclass=50,xlim=c(-4,4))
+hist(x2,nclass=50,xlim=c(-4,4))
+
+
+x1<-rnorm(10000,0,1)
+x2<-rnorm(10000,0,2)
+par(mfrow=c(2,1))
+hist(x1,nclass=50,xlim=c(-6,6))
+hist(x2,nclass=100,xlim=c(-6,6))
+
+###############################################
+# The cars data: plot and summary stat        #
+###############################################
+
+cars
+
+plot(cars$speed,cars$dist,pch="+",xlab="speed",ylab="stopping distance",col=3)
+title("Speed Vs. Stopping diatance",col=2)
+
+dim(cars)
+cars[,1]
+head(cars)
+mean(cars$speed)
+max(cars$speed)
+min(cars$speed)
+
+help(attach)
+
+attach(cars)
+mean(speed)
+max(speed)
+min(speed)
+detach(cars)
+
+
+cor(cars)
+var(cars$speed)
+print(cars)
+print(cars$speed)
